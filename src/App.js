6bdbc0ab70670e77c53fe69b7ac64b8d25c29aa3
ofir <track2@gmail.com>
2020-03-11 22:58:49 +0200

    added routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from 'components/Navbar/Navbar';
 import WeatherDetails from 'views/WeatherDetails/WeatherDetails';
 import Favorites from 'views/Favorites/Favorites';
 
 function App() {
 	return (
-		<div>
+		<Router>
 			<NavBar />
-			<WeatherDetails />
-			<Favorites />
-		</div>
+			<Switch>
+				<Route exact path='/' component={WeatherDetails} />
+				<Route path='/Favorites' component={Favorites} />
+			</Switch>
+		</Router>
 	);
 }
 
