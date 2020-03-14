20ee000d64bee11cc9d9fe471e9b069e4e563be7
ofir <track2@gmail.com>
2020-03-14 15:21:33 +0200

    added loading logic with a skeleton

--- src/components/CurrentWeatherCard/CurrentWeatherCard.js
+++ src/components/CurrentWeatherCard/CurrentWeatherCard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
+import Skeleton from '@material-ui/lab/Skeleton';
 
 import tempWeatherImage from 'assets/WeatherIcons/01-s.png';
 
@@ -9,23 +10,54 @@ const useStyles = makeStyles({
 	root: {
 		display: 'inline-block',
 		margin: '20px',
+		minWidth: '200px',
 	},
 });
 
-export default function CurrentWeatherCard({ loading, location, temperature }) {
+export default function CurrentWeatherCard({
+	loading,
+	location = '',
+	temperature = '',
+}) {
 	const classes = useStyles();
 	return (
 		<Card className={classes.root}>
 			<CardHeader
 				avatar={
-					<img
-						src={tempWeatherImage}
-						alt='image'
-						placeholder='placeholder'
-					/>
+					loading ? (
+						<Skeleton
+							animation='wave'
+							variant='circle'
+							width={40}
+							height={40}
+						/>
+					) : (
+						<img
+							src={tempWeatherImage}
+							alt='image'
+							placeholder='placeholder'
+						/>
+					)
 				}
-				title={location}
-				subheader={temperature}
+				title={
+					loading ? (
+						<Skeleton
+							animation='wave'
+							height={10}
+							width='80%'
+							style={{ marginBottom: 6 }}
+						/>
+					) : (
+						location
+					)
+				}
+				subheader={
+					loading ? (
+						<Skeleton animation='wave' height={10} width='40%' />
+					) : (
+						temperature
+					)
+				}
 			/>
 		</Card>
 	);
