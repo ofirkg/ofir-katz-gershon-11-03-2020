549900cfcd131d50264471c87af618a305d86a07
ofir <track2@gmail.com>
2020-03-14 15:37:01 +0200

    added weather icons index

---
+++ src/assets/WeatherIcons/index.js
@@ -0,0 +1,83 @@
+import icon1 from 'assets/WeatherIcons/01-s.png';
+import icon2 from 'assets/WeatherIcons/02-s.png';
+import icon3 from 'assets/WeatherIcons/03-s.png';
+import icon4 from 'assets/WeatherIcons/04-s.png';
+import icon5 from 'assets/WeatherIcons/05-s.png';
+import icon6 from 'assets/WeatherIcons/06-s.png';
+import icon7 from 'assets/WeatherIcons/07-s.png';
+import icon8 from 'assets/WeatherIcons/08-s.png';
+import icon11 from 'assets/WeatherIcons/11-s.png';
+import icon12 from 'assets/WeatherIcons/12-s.png';
+import icon13 from 'assets/WeatherIcons/13-s.png';
+import icon14 from 'assets/WeatherIcons/14-s.png';
+import icon15 from 'assets/WeatherIcons/15-s.png';
+import icon16 from 'assets/WeatherIcons/16-s.png';
+import icon17 from 'assets/WeatherIcons/17-s.png';
+import icon18 from 'assets/WeatherIcons/18-s.png';
+import icon19 from 'assets/WeatherIcons/19-s.png';
+import icon20 from 'assets/WeatherIcons/20-s.png';
+import icon21 from 'assets/WeatherIcons/21-s.png';
+import icon22 from 'assets/WeatherIcons/22-s.png';
+import icon23 from 'assets/WeatherIcons/23-s.png';
+import icon24 from 'assets/WeatherIcons/24-s.png';
+import icon25 from 'assets/WeatherIcons/25-s.png';
+import icon26 from 'assets/WeatherIcons/26-s.png';
+import icon29 from 'assets/WeatherIcons/29-s.png';
+import icon30 from 'assets/WeatherIcons/30-s.png';
+import icon31 from 'assets/WeatherIcons/31-s.png';
+import icon32 from 'assets/WeatherIcons/32-s.png';
+import icon33 from 'assets/WeatherIcons/33-s.png';
+import icon34 from 'assets/WeatherIcons/34-s.png';
+import icon35 from 'assets/WeatherIcons/35-s.png';
+import icon36 from 'assets/WeatherIcons/36-s.png';
+import icon37 from 'assets/WeatherIcons/37-s.png';
+import icon38 from 'assets/WeatherIcons/38-s.png';
+import icon39 from 'assets/WeatherIcons/39-s.png';
+import icon40 from 'assets/WeatherIcons/40-s.png';
+import icon41 from 'assets/WeatherIcons/41-s.png';
+import icon42 from 'assets/WeatherIcons/42-s.png';
+import icon43 from 'assets/WeatherIcons/43-s.png';
+import icon44 from 'assets/WeatherIcons/44-s.png';
+
+export default {
+	1: icon1,
+	2: icon2,
+	3: icon3,
+	4: icon4,
+	5: icon5,
+	6: icon6,
+	7: icon7,
+	8: icon8,
+	11: icon11,
+	12: icon12,
+	13: icon13,
+	14: icon14,
+	15: icon15,
+	16: icon16,
+	17: icon17,
+	18: icon18,
+	19: icon19,
+	20: icon20,
+	21: icon21,
+	22: icon22,
+	23: icon23,
+	24: icon24,
+	25: icon25,
+	26: icon26,
+	29: icon29,
+	30: icon30,
+	31: icon31,
+	32: icon32,
+	33: icon33,
+	34: icon34,
+	35: icon35,
+	36: icon36,
+	37: icon37,
+	38: icon38,
+	39: icon39,
+	40: icon40,
+	41: icon41,
+	42: icon42,
+	43: icon43,
+	44: icon44,
+};
