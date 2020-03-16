b40eb8553a7b7a57a6f29785b3e13f744da80af2
ofir <track2@gmail.com>
2020-03-16 17:52:19 +0200

    added content to card - icon, phrase, temperatures. added typography. added styles. added default data prop.

--- src/components/DayCard/DayCard.js
+++ src/components/DayCard/DayCard.js
@@ -2,36 +2,58 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
 import Skeleton from '@material-ui/lab/Skeleton';
 import ImageOutlinedIcon from '@material-ui/icons/ImageOutlined';
 import WeatherIcons from 'assets/WeatherIcons/';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
-	root: {
-		// display: 'inline-block',
-		// margin: '20px',
-		// minWidth: '200px',
+	content: {
+		display: 'flex',
+		justifyContent: 'center',
+		marginBottom: '20px',
+	},
+	contentPhrase: {
+		flexGrow: '1',
+	},
+	degrees: {
+		width: '100%',
+		textAlign: 'center',
 	},
 });
 
-export default function CurrentWeatherCard({ loading = true, data }) {
+const defaultData = {
+	Date: Date.now(),
+	Day: {
+		Icon: 0,
+		IconPhrase: '',
+	},
+	Temperature: {
+		Minimum: {
+			Value: 0,
+		},
+		Maximum: {
+			Value: 0,
+		},
+	},
+};
+
+export default function DayCard({ loading = true, data = defaultData }) {
 	const classes = useStyles();
 
-	let dayName, formattedDate;
+	const dayIcon = data.Day.Icon;
+	const IconPhrase = data.Day.IconPhrase;
+	const date = new Date(data.Date);
 
-	if (!loading && data) {
-		const date = new Date(data.Date);
-
-		dayName = date.toLocaleDateString('en-us', {
-			weekday: 'long',
-		});
-
-		const [month, day] = date.toLocaleDateString().split('/');
-		formattedDate = `${day}/${month}`;
-	}
+	const dayName = date.toLocaleDateString('en-us', {
+		weekday: 'long',
+	});
+	const [month, day] = date.toLocaleDateString().split('/');
+	const formattedDate = `${day}/${month}`;
 
 	return (
-		<Card className={classes.root}>
+		<Card>
 			<CardHeader
 				title={
 					loading ? (
@@ -53,40 +75,53 @@ export default function CurrentWeatherCard({ loading = true, data }) {
 					)
 				}
 			/>
+			<CardContent>
+				<div className={classes.content}>
+					<div>
+						{loading ? (
+							<Skeleton
+								animation='wave'
+								variant='circle'
+								width={40}
+								height={40}
+							/>
+						) : WeatherIcons[dayIcon] ? (
+							<img src={WeatherIcons[dayIcon]} alt={IconPhrase} />
+						) : (
+							<ImageOutlinedIcon />
+						)}
+					</div>
+					<div className={classes.contentPhrase}>
+						{loading ? (
+							<Skeleton
+								animation='wave'
+								height={10}
+								width='80%'
+							/>
+						) : (
+							IconPhrase
+						)}
+					</div>
+				</div>
+
+				<div className={classes.degrees}>
+					{loading ? (
+						<Skeleton animation='wave' height={10} width='100%' />
+					) : (
+						<>
+							<Typography variant='h3' component='span'>
+								{data.Temperature.Maximum.Value}&deg;
+							</Typography>
+							<Typography
+								color='textSecondary'
+								variant='h5'
+								component='span'>
+								/{data.Temperature.Maximum.Value}&deg;
+							</Typography>
+						</>
+					)}
+				</div>
+			</CardContent>
 		</Card>
 	);
 }
-
-const dataMock = {
-	Date: '2020-03-14T07:00:00+02:00',
-	EpochDate: 1584162000,
-	Temperature: {
-		Minimum: {
-			Value: 14.2,
-			Unit: 'C',
-			UnitType: 17,
-		},
-		Maximum: {
-			Value: 19,
-			Unit: 'C',
-			UnitType: 17,
-		},
-	},
-	Day: {
-		Icon: 14,
-		IconPhrase: 'Partly sunny w/ showers',
-		HasPrecipitation: true,
-		PrecipitationType: 'Rain',
-		PrecipitationIntensity: 'Light',
-	},
-	Night: {
-		Icon: 37,
-		IconPhrase: 'Hazy moonlight',
-		HasPrecipitation: false,
-	},
-	Sources: ['AccuWeather'],
-	MobileLink:
-		'http://m.accuweather.com/en/il/netanya/212474/daily-weather-forecast/212474?day=1&unit=c&lang=en-us',
-	Link:
-		'http://www.accuweather.com/en/il/netanya/212474/daily-weather-forecast/212474?day=1&unit=c&lang=en-us',
-};
