21ae4fc38a4e4a0c8a3306fea8ba8ad831cffe98
ofir <track2@gmail.com>
2020-03-17 19:20:18 +0200

    add error handling. remove mocks.

--- src/views/WeatherDetails/WeatherDetails.js
+++ src/views/WeatherDetails/WeatherDetails.js
@@ -10,11 +10,13 @@ import Autocomplete from '@material-ui/lab/Autocomplete';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Typography from '@material-ui/core/Typography';
 import Skeleton from '@material-ui/lab/Skeleton';
+import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
+import Card from '@material-ui/core/Card';
 
 axios.defaults.baseURL = 'http://dataservice.accuweather.com';
 const { CancelToken } = axios;
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
 	root: {
 		alignContent: 'start',
 		height: '100%',
@@ -29,10 +31,28 @@ const useStyles = makeStyles({
 	},
 	currentWeatherPhrase: {
 		margin: '50px 0',
+		[theme.breakpoints.down('xs')]: {
+			fontSize: '1.75rem',
+		},
 	},
-});
+	currentWeatherErrorWrapper: {
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+	currentWeatherError: {
+		margin: '20px 0 50px',
+		textAlign: 'center',
+		[theme.breakpoints.down('xs')]: {
+			fontSize: '1.75rem',
+		},
+	},
+	currentWeatherErrorIcon: {
+		fontSize: '50px',
+	},
+}));
 
-const telAvivOption = {
+const defaultWeather = {
 	Version: 1,
 	Key: '215854',
 	Type: 'City',
@@ -54,6 +74,9 @@ export default function WeatherDetails() {
 	const [loading, setLoading] = useState(false);
 	const [currentWeatherLoading, setCurrentWeatherLoading] = useState(false);
 	const [forecastLoading, setForecastLoading] = useState(false);
+	const [autocompleteError, setAutocompleteError] = useState(false);
+	const [currentWeatherError, setCurrentWeatherError] = useState(false);
+	const [forecastError, setForecastError] = useState(false);
 
 	// autocomplete fetch
 	useEffect(() => {
@@ -78,9 +101,15 @@ export default function WeatherDetails() {
 				});
 				setOptions(result.data || []);
 				setLoading(false);
+				setAutocompleteError(false);
 			} catch (e) {
+				if (axios.isCancel(e)) {
+					console.error('fetch cancelled by user');
+				} else {
+					console.error({ e });
+					setAutocompleteError(true);
+				}
 				setLoading(false);
-				console.error(e);
 			}
 		})();
 		return () => source.cancel();
@@ -105,9 +134,15 @@ export default function WeatherDetails() {
 				});
 				setCurrentWeather(result.data[0] || null);
 				setCurrentWeatherLoading(false);
+				setCurrentWeatherError(false);
 			} catch (e) {
+				if (axios.isCancel(e)) {
+					console.error('fetch cancelled by user');
+				} else {
+					console.error({ e });
+					setCurrentWeatherError(true);
+				}
 				setCurrentWeatherLoading(false);
-				console.error(e);
 			}
 		})();
 		return () => source.cancel();
@@ -133,9 +168,15 @@ export default function WeatherDetails() {
 				});
 				setForecast(result.data.DailyForecasts || null);
 				setForecastLoading(false);
+				setForecastError(false);
 			} catch (e) {
+				if (axios.isCancel(e)) {
+					console.error('fetch cancelled by user');
+				} else {
+					console.error({ e });
+					setForecastError(true);
+				}
 				setForecastLoading(false);
-				console.error(e);
 			}
 		})();
 		return () => source.cancel();
@@ -143,37 +184,7 @@ export default function WeatherDetails() {
 
 	// default weather fetch
 	useEffect(() => {
-		// setCurrentWeatherLoading(true);
-		// const source = CancelToken.source();
-
-		setSelectedOption(telAvivOption);
-		// (async () => {
-		// 	try {
-		// 		// const result = await axios({
-		// 		// 	url: `/currentconditions/v1/${selectedOption.Key}`,
-		// 		// 	method: 'GET',
-		// 		// 	cancelToken: source.token,
-		// 		// 	params: {
-		// 		// 		apikey: 'ZykvKfNQRGnZSPw9DdilEwqEzni3OBqb',
-		// 		// 	},
-		// 		// });
-		// 		// setCurrentWeather(result.data[0] || null);
-
-		// 				// 		await new Promise(resolve =>
-		// // 			setTimeout(() => {
-		// // 				setSelectedOption(telavivLocationInfoMock[0]);
-		// // 			}, 3000)
-		// // 		);
-
-		// // 		// setCurrentWeatherLoading(false);
-
-		// 		// setLoading(false);
-		// 	} catch (e) {
-		// 		setCurrentWeatherLoading(false);
-		// 		console.error(e);
-		// 	}
-		// })();
-		// return () => source.cancel();
+		setSelectedOption(defaultWeather);
 	}, []);
 
 	const handleInputChange = (e, term, reason) => {
@@ -241,6 +252,7 @@ export default function WeatherDetails() {
 							<Grid item xs={12} sm={6}>
 								<CurrentWeatherCard
 									loading={currentWeatherLoading}
+									error={currentWeatherError}
 									location={
 										selectedOption &&
 										selectedOption.LocalizedName
@@ -272,6 +284,22 @@ export default function WeatherDetails() {
 									width='40%'
 									style={{ margin: '50px 0' }}
 								/>
+							) : currentWeatherError ? (
+								<div
+									className={
+										classes.currentWeatherErrorWrapper
+									}>
+									<SentimentVeryDissatisfiedIcon
+										className={
+											classes.currentWeatherErrorIcon
+										}
+									/>
+									<Typography
+										variant='h2'
+										className={classes.currentWeatherError}>
+										Information Unavailable
+									</Typography>
+								</div>
 							) : (
 								<Typography
 									variant='h2'
@@ -298,6 +326,7 @@ export default function WeatherDetails() {
 										<DayCard
 											loading={forecastLoading}
 											data={forecast && forecast[i]}
+											error={forecastError}
 										/>
 									</Grid>
 								);
@@ -309,350 +338,3 @@ export default function WeatherDetails() {
 		</Grid>
 	);
 }
-
-const autoCompleteMock = [
-	{
-		Version: 1,
-		Key: '182536',
-		Type: 'City',
-		Rank: 10,
-		LocalizedName: 'Athens',
-		Country: { ID: 'GR', LocalizedName: 'Greece' },
-		AdministrativeArea: { ID: 'I', LocalizedName: 'Attica' },
-	},
-	{
-		Version: 1,
-		Key: '316938',
-		Type: 'City',
-		Rank: 10,
-		LocalizedName: 'Ankara',
-		Country: { ID: 'TR', LocalizedName: 'Turkey' },
-		AdministrativeArea: { ID: '06', LocalizedName: 'Ankara' },
-	},
-	{
-		Version: 1,
-		Key: '126995',
-		Type: 'City',
-		Rank: 11,
-		LocalizedName: 'Alexandria',
-		Country: { ID: 'EG', LocalizedName: 'Egypt' },
-		AdministrativeArea: { ID: 'ALX', LocalizedName: 'Alexandria' },
-	},
-	{
-		Version: 1,
-		Key: '56912',
-		Type: 'City',
-		Rank: 13,
-		LocalizedName: 'Anqing',
-		Country: { ID: 'CN', LocalizedName: 'China' },
-		AdministrativeArea: { ID: 'AH', LocalizedName: 'Anhui' },
-	},
-	{
-		Version: 1,
-		Key: '59083',
-		Type: 'City',
-		Rank: 15,
-		LocalizedName: 'Anyang',
-		Country: { ID: 'CN', LocalizedName: 'China' },
-		AdministrativeArea: { ID: 'HA', LocalizedName: 'Henan' },
-	},
-	{
-		Version: 1,
-		Key: '102138',
-		Type: 'City',
-		Rank: 15,
-		LocalizedName: 'Anshan',
-		Country: { ID: 'CN', LocalizedName: 'China' },
-		AdministrativeArea: { ID: 'LN', LocalizedName: 'Liaoning' },
-	},
-	{
-		Version: 1,
-		Key: '202438',
-		Type: 'City',
-		Rank: 15,
-		LocalizedName: 'Ahmedabad',
-		Country: { ID: 'IN', LocalizedName: 'India' },
-		AdministrativeArea: { ID: 'GJ', LocalizedName: 'Gujarat' },
-	},
-	{
-		Version: 1,
-		Key: '2093',
-		Type: 'City',
-		Rank: 20,
-		LocalizedName: 'Algiers',
-		Country: { ID: 'DZ', LocalizedName: 'Algeria' },
-		AdministrativeArea: { ID: '16', LocalizedName: 'Alger' },
-	},
-	{
-		Version: 1,
-		Key: '126831',
-		Type: 'City',
-		Rank: 20,
-		LocalizedName: 'Addis Ababa',
-		Country: { ID: 'ET', LocalizedName: 'Ethiopia' },
-		AdministrativeArea: { ID: 'AA', LocalizedName: 'Addis Ababa' },
-	},
-	{
-		Version: 1,
-		Key: '178551',
-		Type: 'City',
-		Rank: 20,
-		LocalizedName: 'Accra',
-		Country: { ID: 'GH', LocalizedName: 'Ghana' },
-		AdministrativeArea: { ID: 'AA', LocalizedName: 'Greater Accra' },
-	},
-];
-
-const currentWeatherMock = {
-	LocalObservationDateTime: '2020-03-14T10:05:00+02:00',
-	EpochTime: 1584173100,
-	WeatherText: 'Clouds and sun',
-	WeatherIcon: 4,
-	HasPrecipitation: false,
-	PrecipitationType: null,
-	IsDayTime: true,
-	Temperature: {
-		Metric: {
-			Value: 18.3,
-			Unit: 'C',
-			UnitType: 17,
-		},
-		Imperial: {
-			Value: 65,
-			Unit: 'F',
-			UnitType: 18,
-		},
-	},
-	MobileLink:
-		'http://m.accuweather.com/en/il/netanya/212474/current-weather/212474?lang=en-us',
-	Link:
-		'http://www.accuweather.com/en/il/netanya/212474/current-weather/212474?lang=en-us',
-};
-
-const telavivLocationInfoMock = [
-	{
-		Version: 1,
-		Key: '215854',
-		Type: 'City',
-		Rank: 31,
-		LocalizedName: 'Tel Aviv',
-		Country: {
-			ID: 'IL',
-			LocalizedName: 'Israel',
-		},
-		AdministrativeArea: {
-			ID: 'TA',
-			LocalizedName: 'Tel Aviv',
-		},
-	},
-];
-
-const telavivCurrentWeatherMock = [
-	{
-		LocalObservationDateTime: '2020-03-14T11:30:00+02:00',
-		EpochTime: 1584178200,
-		WeatherText: 'Partly sunny',
-		WeatherIcon: 3,
-		HasPrecipitation: false,
-		PrecipitationType: null,
-		IsDayTime: true,
-		Temperature: {
-			Metric: {
-				Value: 19.2,
-				Unit: 'C',
-				UnitType: 17,
-			},
-			Imperial: {
-				Value: 66,
-				Unit: 'F',
-				UnitType: 18,
-			},
-		},
-		MobileLink:
-			'http://m.accuweather.com/en/il/tel-aviv/215854/current-weather/215854?lang=en-us',
-		Link:
-			'http://www.accuweather.com/en/il/tel-aviv/215854/current-weather/215854?lang=en-us',
-	},
-];
-
-const fiveDayForecastMock = {
-	Headline: {
-		EffectiveDate: '2020-03-17T07:00:00+02:00',
-		EffectiveEpochDate: 1584421200,
-		Severity: 2,
-		Text:
-			'Air quality will be unhealthy for sensitive groups Saturday morning through Monday afternoon',
-		Category: 'rain',
-		EndDate: '2020-03-18T07:00:00+02:00',
-		EndEpochDate: 1584507600,
-		MobileLink:
-			'http://m.accuweather.com/en/il/netanya/212474/extended-weather-forecast/212474?unit=c&lang=en-us',
-		Link:
-			'http://www.accuweather.com/en/il/netanya/212474/daily-weather-forecast/212474?unit=c&lang=en-us',
-	},
-	DailyForecasts: [
-		{
-			Date: '2020-03-14T07:00:00+02:00',
-			EpochDate: 1584162000,
-			Temperature: {
-				Minimum: {
-					Value: 14.2,
-					Unit: 'C',
-					UnitType: 17,
-				},
-				Maximum: {
-					Value: 19,
-					Unit: 'C',
-					UnitType: 17,
-				},
-			},
-			Day: {
-				Icon: 14,
-				IconPhrase: 'Partly sunny w/ showers',
-				HasPrecipitation: true,
-				PrecipitationType: 'Rain',
-				PrecipitationIntensity: 'Light',
-			},
-			Night: {
-				Icon: 37,
-				IconPhrase: 'Hazy moonlight',
-				HasPrecipitation: false,
-			},
-			Sources: ['AccuWeather'],
-			MobileLink:
-				'http://m.accuweather.com/en/il/netanya/212474/daily-weather-forecast/212474?day=1&unit=c&lang=en-us',
-			Link:
-				'http://www.accuweather.com/en/il/netanya/212474/daily-weather-forecast/212474?day=1&unit=c&lang=en-us',
-		},
-		{
-			Date: '2020-03-15T07:00:00+02:00',
-			EpochDate: 1584248400,
-			Temperature: {
-				Minimum: {
-					Value: 12.8,
-					Unit: 'C',
-					UnitType: 17,
-				},
-				Maximum: {
-					Value: 20.4,
-					Unit: 'C',
-					UnitType: 17,
-				},
-			},
-			Day: {
-				Icon: 6,
-				IconPhrase: 'Mostly cloudy',
-				HasPrecipitation: false,
-			},
-			Night: {
-				Icon: 37,
-				IconPhrase: 'Hazy moonlight',
-				HasPrecipitation: false,
-			},
-			Sources: ['AccuWeather'],
-			MobileLink:
-				'http://m.accuweather.com/en/il/netanya/212474/daily-weather-forecast/212474?day=2&unit=c&lang=en-us',
-			Link:
-				'http://www.accuweather.com/en/il/netanya/212474/daily-weather-forecast/212474?day=2&unit=c&lang=en-us',
-		},
-		{
-			Date: '2020-03-16T07:00:00+02:00',
-			EpochDate: 1584334800,
-			Temperature: {
-				Minimum: {
-					Value: 13,
-					Unit: 'C',
-					UnitType: 17,
-				},
-				Maximum: {
-					Value: 21.7,
-					Unit: 'C',
-					UnitType: 17,
-				},
-			},
-			Day: {
-				Icon: 5,
-				IconPhrase: 'Hazy sunshine',
-				HasPrecipitation: false,
-			},
-			Night: {
-				Icon: 35,
-				IconPhrase: 'Partly cloudy',
-				HasPrecipitation: false,
-			},
-			Sources: ['AccuWeather'],
-			MobileLink:
-				'http://m.accuweather.com/en/il/netanya/212474/daily-weather-forecast/212474?day=3&unit=c&lang=en-us',
-			Link:
-				'http://www.accuweather.com/en/il/netanya/212474/daily-weather-forecast/212474?day=3&unit=c&lang=en-us',
-		},
-		{
-			Date: '2020-03-17T07:00:00+02:00',
-			EpochDate: 1584421200,
-			Temperature: {
-				Minimum: {
-					Value: 11.7,
-					Unit: 'C',
-					UnitType: 17,
-				},
-				Maximum: {
-					Value: 19.4,
-					Unit: 'C',
-					UnitType: 17,
-				},
-			},
-			Day: {
-				Icon: 14,
-				IconPhrase: 'Partly sunny w/ showers',
-				HasPrecipitation: true,
-				PrecipitationType: 'Rain',
-				PrecipitationIntensity: 'Light',
-			},
-			Night: {
-				Icon: 39,
-				IconPhrase: 'Partly cloudy w/ showers',
-				HasPrecipitation: true,
-				PrecipitationType: 'Rain',
-				PrecipitationIntensity: 'Light',
-			},
-			Sources: ['AccuWeather'],
-			MobileLink:
-				'http://m.accuweather.com/en/il/netanya/212474/daily-weather-forecast/212474?day=4&unit=c&lang=en-us',
-			Link:
-				'http://www.accuweather.com/en/il/netanya/212474/daily-weather-forecast/212474?day=4&unit=c&lang=en-us',
-		},
-		{
-			Date: '2020-03-18T07:00:00+02:00',
-			EpochDate: 1584507600,
-			Temperature: {
-				Minimum: {
-					Value: 11.6,
-					Unit: 'C',
-					UnitType: 17,
-				},
-				Maximum: {
-					Value: 15.5,
-					Unit: 'C',
-					UnitType: 17,
-				},
-			},
-			Day: {
-				Icon: 3,
-				IconPhrase: 'Partly sunny',
-				HasPrecipitation: false,
-			},
-			Night: {
-				Icon: 35,
-				IconPhrase: 'Partly cloudy',
-				HasPrecipitation: true,
-				PrecipitationType: 'Rain',
-				PrecipitationIntensity: 'Light',
-			},
-			Sources: ['AccuWeather'],
-			MobileLink:
-				'http://m.accuweather.com/en/il/netanya/212474/daily-weather-forecast/212474?day=5&unit=c&lang=en-us',
-			Link:
-				'http://www.accuweather.com/en/il/netanya/212474/daily-weather-forecast/212474?day=5&unit=c&lang=en-us',
-		},
-	],
-};
