9a215d182d5709ff918e06bd9951fce81b455bdf
ofir <track2@gmail.com>
2020-03-18 17:38:10 +0200

    add styles hook and root styles. add grid container. add title.

--- src/views/Favorites/Favorites.js
+++ src/views/Favorites/Favorites.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+	root: {
+		alignContent: 'start',
+		height: '100%',
+	},
+});
 
 export default function Favorites() {
-	return <div></div>;
+	const classes = useStyles();
+	return (
+		<Grid container justify='center' className={classes.root}>
+			<Typography variant='h2'>Favorites</Typography>
+		</Grid>
+	);
 }
