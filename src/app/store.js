c681b6f1de8ace3125e793d2fbaf7d702baa90ce
ofir <track2@gmail.com>
2020-03-18 14:59:21 +0200

    added favorites reducer/actions/selector. added favorites reducer to store.

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import favoritesReducer from 'views/Favorites/FavoritesSlice';
 
 export default configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+	reducer: {
+		favorites: favoritesReducer,
+	},
 });
