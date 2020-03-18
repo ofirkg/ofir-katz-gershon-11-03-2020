946af52c430bdc4cdea6bb93a1e68f7f13a50b2e
ofir <track2@gmail.com>
2020-03-19 00:17:26 +0200

    Updates

---
+++ precache-manifest.6eed7d5e9848e1196dd6d06f1380837b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19a912a6397917d74b6eeaf9c86b686b",
+    "url": "/ofir-katz-gershon-11-03-2020/index.html"
+  },
+  {
+    "revision": "99f585eb2f0a73f9aa12",
+    "url": "/ofir-katz-gershon-11-03-2020/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "75014d3c5e0c056c4cff",
+    "url": "/ofir-katz-gershon-11-03-2020/static/js/2.2814290c.chunk.js"
+  },
+  {
+    "revision": "84c7b19269f6e118b33bddb7c7a89789",
+    "url": "/ofir-katz-gershon-11-03-2020/static/js/2.2814290c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99f585eb2f0a73f9aa12",
+    "url": "/ofir-katz-gershon-11-03-2020/static/js/main.325341df.chunk.js"
+  },
+  {
+    "revision": "dfb9f8da623e22ba09d7",
+    "url": "/ofir-katz-gershon-11-03-2020/static/js/runtime-main.0b33fd54.js"
+  },
+  {
+    "revision": "39ac499c2a7856b24ffe3ffde4c8cc61",
+    "url": "/ofir-katz-gershon-11-03-2020/static/media/15-s.39ac499c.png"
+  },
+  {
+    "revision": "0cf9cad1347244da8a9e197998e0eff9",
+    "url": "/ofir-katz-gershon-11-03-2020/static/media/16-s.0cf9cad1.png"
+  },
+  {
+    "revision": "3c85da3e400a073c51b3ea73bdf5a130",
+    "url": "/ofir-katz-gershon-11-03-2020/static/media/17-s.3c85da3e.png"
+  },
+  {
+    "revision": "421c13f1b1eb0ae947bb64472fec6baa",
+    "url": "/ofir-katz-gershon-11-03-2020/static/media/41-s.421c13f1.png"
+  }
+]);
