5fc2372a149e5dfcb9b6c271d00f9c220ce387c8
ofir <track2@gmail.com>
2020-03-27 12:24:32 +0300

    add dark mode styling. add paper wrapper for the favorite cards.

--- src/views/Favorites/Favorites.js
+++ src/views/Favorites/Favorites.js
@@ -6,21 +6,25 @@ import FavoriteCard from 'components/FavoriteCard/FavoriteCard';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import { Paper } from '@material-ui/core';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
 	root: {
 		alignContent: 'start',
-		height: '100%',
 	},
 	title: {
 		marginBottom: '50px',
 	},
-});
+	paperWrapper: {
+		padding: '20px',
+		height: '100%',
+		backgroundColor: theme.palette.background.paperWrapper,
+	},
+}));
 
 export default function Favorites() {
 	const classes = useStyles();
 	const favorites = useSelector(selectFavorites);
-
 	return (
 		<Grid container justify='center' className={classes.root}>
 			<Grid
@@ -31,22 +35,31 @@ export default function Favorites() {
 				className={classes.title}>
 				<Typography variant='h2'>Favorites</Typography>
 			</Grid>
-			<Grid container justify='center' item xs={10} spacing={3}>
-				{favorites &&
-					favorites.map(fav => {
-						return (
-							<Grid item key={fav.id}>
-								<Link
-									to={`/?id=${fav.id}&location=${fav.location}`}
-									style={{ textDecoration: 'none' }}>
-									<FavoriteCard
-										location={fav.location}
-										{...fav.data}
-									/>
-								</Link>
-							</Grid>
-						);
-					})}
+			<Grid item xs={10}>
+				<Paper className={classes.paperWrapper}>
+					<Grid container spacing={3}>
+						{favorites &&
+							favorites.map(fav => {
+								return (
+									<Grid
+										item
+										xs={12}
+										sm={6}
+										md={2}
+										key={fav.id}>
+										<Link
+											to={`/?id=${fav.id}&location=${fav.location}`}
+											style={{ textDecoration: 'none' }}>
+											<FavoriteCard
+												location={fav.location}
+												{...fav.data}
+											/>
+										</Link>
+									</Grid>
+								);
+							})}
+					</Grid>
+				</Paper>
 			</Grid>
 		</Grid>
 	);
