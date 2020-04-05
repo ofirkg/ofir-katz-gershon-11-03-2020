7248071b4f8f5665d2cef63552ffa9ce0685cc71
ofir <track2@gmail.com>
2020-04-05 22:23:35 +0300

    added redux-persist to handle redux local storage persistence and rehydration.

--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,33 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+
 import favoritesReducer from 'views/Favorites/FavoritesSlice';
 import weatherDetailsReducer from 'views/WeatherDetails/WeatherDetailsSlice';
 import themeReducer from 'AppSlice';
 import temperatureToggleReducer from 'components/TemperatureToggle/TemperatureToggleSlice';
 
-export default configureStore({
-	reducer: {
-		favorites: favoritesReducer,
-		weatherDetails: weatherDetailsReducer,
-		theme: themeReducer,
-		temperatureScale: temperatureToggleReducer,
-	},
+const persistConfig = {
+	key: 'root',
+	storage: storage,
+	stateReconciler: autoMergeLevel2,
+};
+
+const rootReducer = combineReducers({
+	favorites: favoritesReducer,
+	weatherDetails: weatherDetailsReducer,
+	theme: themeReducer,
+	temperatureScale: temperatureToggleReducer,
 });
+
+const pReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+	reducer: pReducer,
+});
+
+export const persistor = persistStore(store);
+
+export default store;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import store from './app/store';
+import { store, persistor } from './app/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import * as serviceWorker from './serviceWorker';
+import { persistStore } from 'redux-persist';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+	<Provider store={store}>
+		<PersistGate loading={null} persistor={persistor}>
+			<App />
+		</PersistGate>
+	</Provider>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
